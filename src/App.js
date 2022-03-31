8225d4ed20c5a479c42dd1b47130d28eeb7c0553
Skyz03 <aakibdiscoo@gmail.com>
2022-03-31 20:09:06 +0545

    Updates in the die component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import Die from "./components/Die";
 
 function App() {
   return (
     <main>
-      <h1>Tenzies</h1>
+      <div className="die">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
     </main>
   );
 }
